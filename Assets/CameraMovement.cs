00ad0a3c4ebf380ad28f1411cbb71beb2f3da652
Alan Widmer <alan.widmer@yahoo.com>
2013-12-18 20:34:04 -0800

    First version
    At the conclusion of the training sessions with Curtis

---
+++ Assets/CameraMovement.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour
+{
+	public Transform transformToFollow;
+	public Vector3 v3fFollowingOffset;
+	
+	// Called at the end of every frame
+	void LateUpdate()
+	{
+		transform.position = transformToFollow.position + v3fFollowingOffset;
+	}
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour 
+{
+	public float fSpeed = 100.0f;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void Update()
+	{
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+		
+		float fMoveHorizontal = Input.GetAxis("Horizontal");
+		float fMoveVertical = Input.GetAxis("Vertical");
+		Vector3 v3fMoveForce = new Vector3(fMoveHorizontal, 0.0f, fMoveVertical) * Time.fixedDeltaTime * fSpeed;
+// 		print ("v3fMoveForce = " + v3fMoveForce);
+		rigidbody.AddForce(v3fMoveForce);
+	}
+}
